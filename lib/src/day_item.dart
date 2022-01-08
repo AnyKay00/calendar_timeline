85871590347d9d1f09d8f61e76d90520499572cb
Annika <annika.latz@web.de>
2022-01-08 10:03:06 +0100

    Changed Fontsize of Days.

--- lib/src/day_item.dart
+++ lib/src/day_item.dart
@@ -36,11 +36,11 @@ class DayItem extends StatelessWidget {
         ? dayColor ?? Theme.of(context).colorScheme.secondary
         : dayColor?.withOpacity(0.5) ??
         Theme.of(context).colorScheme.secondary.withOpacity(0.5),
-      fontSize: 32,
+      fontSize: 24,
       fontWeight: FontWeight.normal);
     final selectedStyle = TextStyle(
       color: activeDayColor ?? Colors.white,
-      fontSize: 32,
+      fontSize: 24,
       fontWeight: FontWeight.bold,
       height: 0.8,
     );
