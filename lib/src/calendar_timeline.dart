967c02ec5e0226a3d4f87b3fa313c062c8c3be9e
Annika <annika.latz@web.de>
2022-01-14 20:09:09 +0100

    Set scrollallignment

--- lib/src/calendar_timeline.dart
+++ lib/src/calendar_timeline.dart
@@ -109,7 +109,7 @@ class _CalendarTimelineState extends State<CalendarTimeline> {
 
   @override
   Widget build(BuildContext context) {
-    _scrollAlignment = widget.leftMargin / MediaQuery.of(context).size.width;
+    _scrollAlignment = 2 / MediaQuery.of(context).size.width;
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
